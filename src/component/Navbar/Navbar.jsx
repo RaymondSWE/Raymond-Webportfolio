4b52372d0c515ebebb9324b383bbf627e5646fb0
RaymondSWE <ramanassaredsskolan@hotmail.com>
2023-07-06 22:54:40 +0200

    nvabar

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -1,7 +1,8 @@
-import React from 'react';
-import './Navbar.css'; 
+import React, { useEffect, useRef } from "react";
+import "./Navbar.css";
 
 const Navbar = () => {
+  const ref = useRef(null);
   const handleClick = (e) => {
     e.preventDefault();
     const target = e.target.getAttribute("href");
@@ -11,28 +12,75 @@ const Navbar = () => {
     }
   };
 
+
+  
+  useEffect(() => {
+    const listItems = ref.current.querySelectorAll(".navbar-container-navbar-item");
+    listItems.forEach((item, index) => {
+      item.style.setProperty("--i", index);
+    });
+  }, []);
+
   return (
     <nav className="navbar">
-      <div className="navbar-container">
-        <a href="#about" className="navbar-container-navbar-logo">Raman Mohammed</a>
+      <div className="navbar-container" ref={ref}>
+        <a href="#about" className="navbar-container-navbar-logo">
+          Raman Mohammed
+        </a>
         <ul className="navbar-container-navbar-menu">
           <li className="navbar-container-navbar-item">
-            <a href="#about" className="navbar-container-navbar-links" onClick={handleClick}>About</a>
+            <a
+              href="#about"
+              className="navbar-container-navbar-links"
+              onClick={handleClick}
+            >
+              About
+            </a>
           </li>
           <li className="navbar-container-navbar-item">
-            <a href="#skills" className="navbar-container-navbar-links" onClick={handleClick}>Skills</a>
+            <a
+              href="#skills"
+              className="navbar-container-navbar-links"
+              onClick={handleClick}
+            >
+              Skills
+            </a>
           </li>
           <li className="navbar-container-navbar-item">
-            <a href="#projects" className="navbar-container-navbar-links" onClick={handleClick}>Projects</a>
+            <a
+              href="#projects"
+              className="navbar-container-navbar-links"
+              onClick={handleClick}
+            >
+              Projects
+            </a>
           </li>
           <li className="navbar-container-navbar-item">
-            <a href="#workexperience" className="navbar-container-navbar-links" onClick={handleClick}>Work</a>
+            <a
+              href="#workexperience"
+              className="navbar-container-navbar-links"
+              onClick={handleClick}
+            >
+              Work
+            </a>
           </li>
           <li className="navbar-container-navbar-item">
-            <a href="#education" className="navbar-container-navbar-links" onClick={handleClick}>Education</a>
+            <a
+              href="#education"
+              className="navbar-container-navbar-links"
+              onClick={handleClick}
+            >
+              Education
+            </a>
           </li>
           <li className="navbar-container-navbar-item">
-            <a href="#contact" className="navbar-container-navbar-links" onClick={handleClick}>Contact</a>
+            <a
+              href="#contact"
+              className="navbar-container-navbar-links"
+              onClick={handleClick}
+            >
+              Contact
+            </a>
           </li>
         </ul>
       </div>
