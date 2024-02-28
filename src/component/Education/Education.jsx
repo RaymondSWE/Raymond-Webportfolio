d77447372989d794ca1c2d988f207f0b9c3b0d4f
RaymondSWE <RamanMohammed@hotmail.com>
2024-02-28 17:28:11 +0100

    env example comment added to clearify the bad naming convention for envs

--- src/component/Education/Education.jsx
+++ src/component/Education/Education.jsx
@@ -2,29 +2,11 @@ import React, { useState, useEffect } from "react";
 import "./Education.css";
 import AOS from "aos";
 import "aos/dist/aos.css"; 
-import Skovde from "../../img/logo/UniversitySkovde.png";
-import Boras from "../../img/logo/BorasLogo.jpg";
 import { TimelineItem } from "../../ui/TimelineItem";
+import { EducationData } from "../../data/EducationData";
 
 const Education = () => {
-  const education = [
-    {
-      title: "University Skövde",
-      subtitle: "Bachelor's in Computer Science",
-      startDate: "August 2021",
-      endDate: "May 2024",
-      description: "Specialization in System Development",
-      logo: Skovde,
-    },
-    {
-      title: "University Borås",
-      subtitle: "Bachelor's in Economics (30 credits)",
-      startDate: "August 2020",
-      endDate: "Jan 2021",
-      description: "Drop out after first term",
-      logo: Boras,
-    },
-  ];
+
 
   useEffect(() => {
     AOS.init({ duration: 2000 });
@@ -36,7 +18,7 @@ const Education = () => {
         Education
       </h2>
       <div className="education-section-timeline">
-        {education.map((item, index) => (
+        {EducationData.map((item, index) => (
           <TimelineItem key={index} {...item} />
         ))}
       </div>
---
+++ src/data/EducationData.js
@@ -0,0 +1,22 @@
+
+import Skovde from "../img/logo/UniversitySkovde.png";
+import Boras from "../img/logo/BorasLogo.jpg";
+
+export const EducationData = [
+    {
+      title: "University Skövde",
+      subtitle: "Bachelor's in Computer Science",
+      startDate: "August 2021",
+      endDate: "May 2024",
+      description: "Specialization in System Development",
+      logo: Skovde,
+    },
+    {
+      title: "University Borås",
+      subtitle: "Bachelor's in Economics (30 credits)",
+      startDate: "August 2020",
+      endDate: "Jan 2021",
+      description: "Drop out after first term",
+      logo: Boras,
+    },
+  ];
